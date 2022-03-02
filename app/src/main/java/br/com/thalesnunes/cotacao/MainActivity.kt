3f71d4b15e0b1f595651f07825c6fe30322fad28
Thales Nunes <thales.o.nunes@gmail.com>
2022-03-02 14:42:28 -0300

    Capturando os valores e tratando possíveis erros de entrada

--- app/src/main/java/br/com/thalesnunes/cotacao/MainActivity.kt
+++ app/src/main/java/br/com/thalesnunes/cotacao/MainActivity.kt
@@ -2,10 +2,48 @@ package br.com.thalesnunes.cotacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        result = findViewById<TextView>(R.id.txt_result)
+
+        val buttonConverter = findViewById<Button>(R.id.btn_converter)
+
+        buttonConverter.setOnClickListener{
+            converter()
+        }
+    }
+
+    private fun converter(){
+        val selectedCurrency = findViewById<RadioGroup>(R.id.radio_group)
+        val checked = selectedCurrency.checkedRadioButtonId
+
+        val currency = when (checked){
+            R.id.radio_usd -> {
+                "USD"
+            }
+            R.id.radio_eur -> {
+                "EUR"
+            }
+            else -> {
+                "CLP"
+            }
+        }
+
+        val editField = findViewById<EditText>(R.id.edit_field)
+        val value = editField.text.toString()
+
+        if(value.isEmpty()){
+            return
+        }
+        
     }
 }
